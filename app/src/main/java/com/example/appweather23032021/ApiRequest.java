bc3e7d470fac4b1fcf6118676b4c904ef8646f8c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-24 20:49:16 +0700

    demo1

--- app/src/main/java/com/example/appweather23032021/ApiRequest.java
+++ app/src/main/java/com/example/appweather23032021/ApiRequest.java
@@ -1,10 +1,9 @@
 package com.example.appweather23032021;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiRequest {
     @GET("KhoaPhamTraining/json/tien/demo1.json")
-    Call<ResponseBody> getDemo1();
+    Call<Demo1> getDemo1();
 }
--- app/src/main/java/com/example/appweather23032021/MainActivity.java
+++ app/src/main/java/com/example/appweather23032021/MainActivity.java
@@ -2,9 +2,19 @@ package com.example.appweather23032021;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -15,15 +25,18 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mImgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Bước 1 : Khởi tạo Retrofit
+        mImgView = findViewById(R.id.imageView);
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                             .connectTimeout(10 , TimeUnit.SECONDS)
@@ -33,24 +46,35 @@ public class MainActivity extends AppCompatActivity {
                             .protocols(Arrays.asList(Protocol.HTTP_1_1))
                             .build();
 
+        Gson gson = new GsonBuilder().setLenient().create();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://khoapham.vn/")
                 .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
         ApiRequest apiRequest = retrofit.create(ApiRequest.class);
 
-        Call<ResponseBody> callback = apiRequest.getDemo1();
+        Call<Demo1> callback = apiRequest.getDemo1();
 
-        callback.enqueue(new Callback<ResponseBody>() {
+        callback.enqueue(new Callback<Demo1>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Log.d("BBB",response.body().toString());
+            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+                Demo1 demo1 = response.body();
+                Glide.with(MainActivity.this)
+                        .load(demo1.getLogo())
+                        .fitCenter()
+                        .override(300 , 200)
+                        .placeholder(R.mipmap.ic_launcher)
+                        .error(android.R.drawable.stat_notify_error)
+                        .into(mImgView);
+
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.d("BBB",t.getMessage().toString());
+            public void onFailure(Call<Demo1> call, Throwable t) {
+
             }
         });
 
